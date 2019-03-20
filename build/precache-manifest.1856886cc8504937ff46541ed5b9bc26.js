d86e9f98b4cc7553f1e507824da860b516bea884
Milos <cosma203@outlook.com>
2019-03-20 17:31:53 +0800

    First Commit

---
+++ build/precache-manifest.1856886cc8504937ff46541ed5b9bc26.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "49add454abb9b699beb6",
+    "url": "/static/css/main.0d9f3e2c.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "fbd04a944d8bc23e41ad",
+    "url": "/static/js/1.fbd04a94.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "49add454abb9b699beb6",
+    "url": "/static/js/main.49add454.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fbd04a944d8bc23e41ad",
+    "url": "/static/css/1.1b8230ba.chunk.css"
+  },
+  {
+    "revision": "ce7c00bd1253f237d638290b7b40015a",
+    "url": "/index.html"
+  }
+];
